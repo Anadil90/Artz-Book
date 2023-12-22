29448d2bc3fa0027245b0f4122ebd0d1e273206a
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2023-12-22 15:02:50 +0100

    backend server setup database connect string

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 require('dotenv').config;
 
 const port = 3000 || process.env.port
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log(`backend server running on port ${port}`)
 })
