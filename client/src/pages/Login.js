e80ceb44faa9ce4f4c9cb71ad3b8aeea306e004f
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2023-12-26 20:38:39 +0100

    login and registration forms styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,28 @@
-
+import { Form, Input, Button } from 'antd';
 
 const Login = () => {
+
     return (
-        <div>
-            login page
+        <div className="auth">
+            <div className="register card p-3">
+                <h1 className="form-title">Login to your user account</h1>
+
+                <Form layout="vertical">
+
+                    <Form.Item label="Email" name="email">
+                        <Input placeholder="Name" />
+                    </Form.Item>
+
+                    <Form.Item label="Password" name="password" type="password">
+                        <Input placeholder="Name" />
+                    </Form.Item>
+
+                    <Button type="primary" className="register-btn mt-3" htmlType="submit">Sign in</Button>
+                </Form>
+            </div>
         </div>
     )
+    
 }
 
 export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,17 @@
 import { Form, Input, Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const onFinish = (values) => {
+        console.log('form values: ', values)
+    }
+
     return (
         <div className="auth">
             <div className="register card p-3">
                 <h1 className="form-title">Register for your user account</h1>
 
-                <Form layout="vertical">
+                <Form layout="vertical" onFinish={onFinish}>
                     <Form.Item label="Name" name="name">
                         <Input placeholder="Name" />
                     </Form.Item>
@@ -15,11 +20,12 @@ const Register = () => {
                         <Input placeholder="Name" />
                     </Form.Item>
 
-                    <Form.Item label="Password" name="password">
+                    <Form.Item label="Password" name="password" type="password">
                         <Input placeholder="Name" />
                     </Form.Item>
 
-                    <Button type="primary" className="register-btn mt-3">Register</Button>
+                    <Button type="primary" className="register-btn mt-3" htmlType="submit">Register</Button>
+                    <Link to="/login" className="my-2">Login to your account</Link>
                 </Form>
             </div>
         </div>
